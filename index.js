da2c8424fb0186a004235406a2900634bbc0098c
NineWoranop <nineworanop@gmail.com>
2020-10-31 17:31:35 +0700

    Update for hosting on Vercel.com

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express')
-const path = require('path')
+const app = express();
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/', (req, res) => {
+  res.send("This is a sample express app")
+})
+
+app.get('/api/users', (req, res) => {
+  res.json({
+    users: [
+      {
+        id: "01",
+        name: "User AAA"
+      }, {
+        id: "02",
+        name: "User BBB"
+      }
+    ]
+
+  })
+})
+
+// Listen on port 5000
+app.listen(PORT, () => {
+  console.log(`Server is booming on port 5000
+Visit http://localhost:5000`);
+});
